c6ebbaf5fa3158317d149b5d81b29efaac26a203
yndgow <yndgow@gmail.com>
2023-01-17 01:01:53 +0900

    2023/01/17 1546

--- 1546.py
+++ 1546.py
@@ -1,5 +1,9 @@
 import sys
 cnt = int(input())
 nlist = list(map(int, sys.stdin.readline().rstrip().split()))
+maxScore = max(nlist)
+totalScore = 0
 for n in nlist:
-    pass
+    n = n / maxScore * 100
+    totalScore += n
+print(totalScore/cnt)
