c19dfdedd932088c16c34276c85239567a0528d8
yndgow <yndgow@gmail.com>
2023-10-31 01:30:39 +0900

    dataread

--- greenstock/dataread.py
+++ greenstock/dataread.py
@@ -3,7 +3,6 @@ import mplfinance as mpf
 from mplfinance.original_flavor import candlestick_ohlc
 import datetime
 from dateutil.relativedelta import relativedelta
-import yfinance as yf
 import matplotlib.dates as mpdates
 import matplotlib.pyplot as plt
 
@@ -12,7 +11,7 @@ if __name__ == '__main__':
 
     name = '005930'
     end_date = datetime.datetime.today()
-    start_date = end_date - relativedelta(months=1)
+    start_date = end_date - relativedelta(days=1)
     df = fdr.DataReader(name, start_date, end_date)
     print(df)
     df['Close'].plot()
@@ -20,7 +19,7 @@ if __name__ == '__main__':
     # df_show['Date'] = df_show.index
     # df_show['Date'] = df_show['Date'].map(mpdates.date2num)
     # df_show = df_show[['Date', 'Open', 'High', 'Low','Close']]
-    
+
     # fig, ax = plt.subplots()
     # candlestick_ohlc(ax, df_show.values, width=0.6, colordown='b', colorup='r', alpha=1)
     # ax.grid(True)
